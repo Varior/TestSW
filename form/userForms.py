07b396fd7bf239d2a03606038d5d8708559ce4aa
Varior <alexandrvenzor@gmail.com>
2017-09-29 16:04:17 +0300

    form validation

--- form/userForms.py
+++ form/userForms.py
@@ -2,7 +2,7 @@ from wtforms import Form, StringField, SelectField, validators
 from wtforms.validators import Required, Email, Regexp
 
 class UserForm(Form):
-    name = StringField('Name', validators = [Required(), Regexp('^[a-z]{1}[a-z\s]+')])
+    name = StringField('Name', validators = [Required(), Regexp('^[a-z]{1}[a-z\s]+$')])
     email = StringField('E-mail', validators = [Required(),Email()])
     phone = StringField('Phone', validators = [Regexp('^\+[0-9]{5,15}')])
     m_phone = StringField('Mobile phone', validators = [Regexp('^\+[0-9]{5,15}')])
