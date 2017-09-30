27526e0fa7dfd54cd0bc7400570e6129121675be
Varior <alexandrvenzor@gmail.com>
2017-09-30 12:21:22 +0300

    Css1

--- form/userForms.py
+++ form/userForms.py
@@ -2,8 +2,8 @@ from wtforms import Form, StringField, SelectField, validators
 from wtforms.validators import Required, Email, Regexp
 
 class UserForm(Form):
-    name = StringField('Name', validators = [Required(), Regexp('^[a-z]{1}[a-z\s]+$')])
-    email = StringField('E-mail', validators = [Required(),Email()])
+    name = StringField('*Name', validators = [Required(), Regexp('^[a-z]{1}[a-z\s]+$')])
+    email = StringField('*E-mail', validators = [Required(),Email()])
     phone = StringField('Phone')
     m_phone = StringField('Mobile phone')
     status = SelectField('Status', coerce=int, choices=[(1, 'Active'),(0, 'Inactive')], default=0)
